c465eccd1b4c88c9f6c06c70b19d29656f2eda6e
lkbobadi <lkbobadi@gmail.com>
2022-02-16 01:03:00 +0900

    Main CSS
    Updated for Main Weather

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container '>
+      <div className='weather-app mt-5'>
+        <form className='search-weather'>
+          <input text='search' id='search' placeholder='Search a city...' />
+          <input type='submit' value='Search' id='submit' />
+        </form>
+        <div className='row'>
+          <ul className='today-extra-info col-1'>
+            <li>Wednesday</li>
+            <li>Sunny</li>
+            <li>3:40PM</li>
+          </ul>
+        </div>
+
+        <div className='main-information'>
+          <h1 className='city'>New York</h1>
+          <div className='icon'>SUN</div>
+          <div className='button-set'>
+            <button className='celsius'>C</button>
+            <button className='farenheit'>F</button>
+          </div>
+        </div>
+        <div className='weather-extra-information  mb-5'>
+          <li>Humidity</li>
+          <li>Wind</li>
+          <li>Pressure</li>
+        </div>
+        <div className='forecast'>
+          Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
