78da1ef5f9f7d30778d88f141ece163c9cd4a7ed
lkbobadi <lkbobadi@gmail.com>
2022-02-17 00:56:26 +0900

    Main JS added
    Main temperature JS added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,84 @@
 import "./App.css";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
 
 export default function App() {
+  let [city, setCity] = useState("New York");
+  let [weatherData, setWeatherData] = useState({});
+
+  function getWeather(response) {
+    setWeatherData({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].main,
+      country: response.data.sys.country,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function updateCity(e) {
+    setCity(e.target.value);
+  }
+
+  function submitCity(e) {
+    e.preventDefault();
+
+    let apiKey = "094780c710fa4efd669f0df8c3991927";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(getWeather);
+    console.log(apiUrl);
+  }
+
   return (
     <div className='container '>
       <div className='weather-app mt-5'>
         <form className='search-weather'>
-          <input text='search' id='search' placeholder='Search a city...' />
-          <input type='submit' value='Search' id='submit' />
+          <input
+            text='search'
+            id='search'
+            placeholder='Search for a city...'
+            onChange={updateCity}
+          />
+          <input
+            type='submit'
+            value='Search'
+            id='submit'
+            onClick={submitCity}
+          />
         </form>
         <div className='row'>
           <ul className='today-extra-info col-1'>
             <li>Wednesday</li>
-            <li>Sunny</li>
+            <li>{weatherData.description}</li>
             <li>3:40PM</li>
           </ul>
         </div>
-
         <div className='main-information'>
-          <h1 className='city'>New York</h1>
-          <div className='icon'>SUN</div>
+          <h1 className='city'>{city}</h1>
+          <div className='icon'>
+            <img src={weatherData.icon} alt={weatherData.description} />
+          </div>
+          <div className='temperature'>
+            {Math.round(weatherData.temperature)}°C
+          </div>
           <div className='button-set'>
             <button className='celsius'>C</button>
             <button className='farenheit'>F</button>
           </div>
         </div>
         <div className='weather-extra-information  mb-5'>
-          <li>Humidity</li>
-          <li>Wind</li>
-          <li>Pressure</li>
+          <li>Country: {weatherData.country}</li>
+          <li>Humidity: {weatherData.humidity}%</li>
+          <li>Wind: {Math.round(weatherData.wind)} km/h</li>
         </div>
-        <div className='forecast'>
-          Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+
+        <div className='footer'>
+          Open-Source by <span>Laura Bobadilla</span>
         </div>
       </div>
     </div>
