152a5202c024d2e5026e462e007cf013208b7193
lkbobadi <lkbobadi@gmail.com>
2022-02-18 13:02:00 +0900

    FormattedWeather Component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import React, { useState } from "react";
+import FormattedWeather from "./FormattedWeather";
 import "bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
 
 export default function App() {
-  let [city, setCity] = useState("New York");
-  let [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
 
   function getWeather(response) {
+    setLoaded(true);
     setWeatherData({
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
@@ -16,6 +19,7 @@ export default function App() {
       description: response.data.weather[0].main,
       country: response.data.sys.country,
       icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      date: new Date(response.data.dt * 1000),
     });
   }
 
@@ -34,53 +38,78 @@ export default function App() {
     console.log(apiUrl);
   }
 
-  return (
-    <div className='container '>
-      <div className='weather-app mt-5'>
-        <form className='search-weather'>
-          <input
-            text='search'
-            id='search'
-            placeholder='Search for a city...'
-            onChange={updateCity}
-          />
-          <input
-            type='submit'
-            value='Search'
-            id='submit'
-            onClick={submitCity}
-          />
-        </form>
-        <div className='row'>
-          <ul className='today-extra-info col-1'>
-            <li>Wednesday</li>
-            <li>{weatherData.description}</li>
-            <li>3:40PM</li>
-          </ul>
-        </div>
-        <div className='main-information'>
-          <h1 className='city'>{city}</h1>
-          <div className='icon'>
-            <img src={weatherData.icon} alt={weatherData.description} />
+  if (loaded) {
+    return (
+      <div className='container '>
+        <div className='weather-app mt-5'>
+          <form className='search-weather'>
+            <input
+              text='search'
+              id='search'
+              placeholder='Search for a city...'
+              onChange={updateCity}
+            />
+            <input
+              type='submit'
+              value='Search'
+              id='submit'
+              onClick={submitCity}
+            />
+          </form>
+          <div className='row'>
+            <ul className='today-extra-info col-1'>
+              <li>
+                <FormattedWeather date={weatherData.data} />
+              </li>
+              <li>{weatherData.description}</li>
+              <li>3:40PM</li>
+            </ul>
           </div>
-          <div className='temperature'>
-            {Math.round(weatherData.temperature)}°C
+          <div className='main-information'>
+            <h1 className='city'>{city}</h1>
+            <div className='icon'>
+              <img src={weatherData.icon} alt={weatherData.description} />
+            </div>
+            <div className='temperature'>
+              {Math.round(weatherData.temperature)}°C
+            </div>
+            <div className='button-set'>
+              <button className='celsius'>C</button>
+              <button className='farenheit'>F</button>
+            </div>
           </div>
-          <div className='button-set'>
-            <button className='celsius'>C</button>
-            <button className='farenheit'>F</button>
+          <div className='weather-extra-information  mb-5'>
+            <li>Country: {weatherData.country}</li>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {Math.round(weatherData.wind)} km/h</li>
           </div>
-        </div>
-        <div className='weather-extra-information  mb-5'>
-          <li>Country: {weatherData.country}</li>
-          <li>Humidity: {weatherData.humidity}%</li>
-          <li>Wind: {Math.round(weatherData.wind)} km/h</li>
-        </div>
 
-        <div className='footer'>
-          Open-Source by <span>Laura Bobadilla</span>
+          <div className='footer'>
+            Open-Source by <span>Laura Bobadilla</span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className='container '>
+        <div className='weather-app mt-5'>
+          <form className='search-weather'>
+            <input
+              text='search'
+              id='search'
+              placeholder='Search for a city...'
+              onChange={updateCity}
+            />
+            <input
+              type='submit'
+              value='Search'
+              id='submit'
+              onClick={submitCity}
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
