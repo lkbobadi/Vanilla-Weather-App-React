39f56185584f5c9aa532fac28bda9380335aceeb
lkbobadi <lkbobadi@gmail.com>
2022-02-19 23:13:41 +0900

    onSubmit error identified, css container changed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import FormattedWeather from "./FormattedWeather";
 import FormattedTime from "./FormattedTime";
 import "bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
+import { eventWrapper } from "@testing-library/user-event/dist/utils";
 
 export default function App() {
   const [city, setCity] = useState("New York");
@@ -23,122 +24,75 @@ export default function App() {
     });
     setLoaded(true);
   }
-
-  function updateCity(e) {
-    setCity(e.target.value);
+  function submitCity(event) {
+    event.preventDefault();
+    search();
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
   }
 
-  function submitCity(e) {
-    e.preventDefault();
-
+  function search() {
     let apiKey = "094780c710fa4efd669f0df8c3991927";
     let units = "metric";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
     axios.get(apiUrl).then(getWeather);
-    console.log(apiUrl);
   }
 
   if (loaded) {
     return (
-      <div className='container '>
-        <div className='weather-app mt-5'>
-          <form className='search-weather'>
-            <input
-              text='search'
-              id='search'
-              placeholder='Search for a city...'
-              onChange={updateCity}
-            />
-            <input
-              type='submit'
-              value='Search'
-              id='submit'
-              onClick={submitCity}
-            />
-          </form>
-          <div className='row'>
-            <ul className='today-extra-info col-1'>
-              <li>
-                <FormattedWeather date={weatherData.date} />
-              </li>
-              <li>{weatherData.description}</li>
-              <li>
-                <FormattedTime date={weatherData.date} />
-              </li>
-            </ul>
-          </div>
-          <div className='main-information'>
-            <h1 className='city'>{city}</h1>
-            <div className='icon'>
-              <img src={weatherData.icon} alt={weatherData.description} />
+      <div className='container'>
+        <div class='g-0'>
+          <div className='weather-app mt-3'>
+            <form className='search-weather' onSubmit={submitCity}>
+              <input
+                text='search'
+                id='search'
+                placeholder='Search for a city...'
+                onChange={updateCity}
+              />
+              <input type='submit' value='Search' id='submit' />
+            </form>
+            <div className='row'>
+              <ul className='today-extra-info col-1'>
+                <li>
+                  <FormattedWeather date={weatherData.date} />
+                </li>
+                <li>{weatherData.description}</li>
+                <li>
+                  <FormattedTime date={weatherData.date} />
+                </li>
+              </ul>
             </div>
-            <div className='temperature'>
-              {Math.round(weatherData.temperature)}°C
+            <div className='main-information'>
+              <h1 className='city'>{city}</h1>
+              <div className='icon'>
+                <img src={weatherData.icon} alt={weatherData.description} />
+              </div>
+              <div className='temperature'>
+                {Math.round(weatherData.temperature)}°C
+              </div>
+              <div className='button-set'>
+                <button className='celsius'>C</button>
+                <button className='farenheit'>F</button>
+              </div>
             </div>
-            <div className='button-set'>
-              <button className='celsius'>C</button>
-              <button className='farenheit'>F</button>
+            <div className='weather-extra-information  mb-5'>
+              <li>Country: {weatherData.country}</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {Math.round(weatherData.wind)} km/h</li>
             </div>
-          </div>
-          <div className='weather-extra-information  mb-5'>
-            <li>Country: {weatherData.country}</li>
-            <li>Humidity: {weatherData.humidity}%</li>
-            <li>Wind: {Math.round(weatherData.wind)} km/h</li>
-          </div>
 
-          <div className='footer'>
-            Open-Source by <span>Laura Bobadilla</span>
+            <div className='footer'>
+              Open-Source by <span>Laura Bobadilla</span>
+            </div>
           </div>
         </div>
       </div>
     );
   } else {
-    return (
-      <div className='container '>
-        <div className='weather-app mt-5'>
-          <form className='search-weather'>
-            <input
-              text='search'
-              id='search'
-              placeholder='Search for a city...'
-              onChange={updateCity}
-            />
-            <input
-              type='submit'
-              value='Search'
-              id='submit'
-              onClick={submitCity}
-            />
-          </form>
-
-          <div className='row'>
-            <ul className='today-extra-info col-1'>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-          </div>
-          <div className='main-information'>
-            <h1 className='city'>{city}</h1>
-            <div className='icon'>{setWeatherData.icon}</div>
-            <div className='temperature'></div>
-            <div className='button-set'>
-              <button className='celsius'>C</button>
-              <button className='farenheit'>F</button>
-            </div>
-          </div>
-          <div className='weather-extra-information  mb-5'>
-            <li>Country: </li>
-            <li>Humidity: </li>
-            <li>Wind: </li>
-          </div>
-
-          <div className='footer'>
-            Open-Source by <span>Laura Bobadilla</span>
-          </div>
-        </div>
-      </div>
-    );
+    submitCity();
+    return "Loading...";
   }
 }
